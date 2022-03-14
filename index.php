060201a437e7b6f9503edee48c511f053679a724
VictorBocNeg <52902590+VictorBocNeg@users.noreply.github.com>
2022-03-13 20:13:42 -0600

    Primer push
    Es la primera versión de la entrega, de no haber correcciones entonces será la última también.

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Vaccination</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='estilos.css'>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="main.js"></script>
+    <script src="app.js"></script>
+
+</head>
+
+<body>
+
+<header>
+    <div class="contenedor">
+
+        <nav class="menu">
+                <a href="#"><h1 id="top-menu">Vaccination</h1></a>
+                <ul id="top-menu">
+                    <li><a href="#">Database</a></li>
+                    <li><a href="#">Education</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Regulation</a></li>
+                    <li><a href="#">Hoax Buster</a></li>
+                </ul>
+        </nav>
+    </div>
+</header>
+
+<div class="contenedor">
+
+    <div class="bloque1">
+        <img id="enfermera" src="img/Enfermera.png">
+    </div>
+    <div>
+        <div class="bloque1" id="espaciados">
+            <p id="fight">Fight Virus<br>With Vaccines</p>
+            <p id="stop">Let's stop this pandemic by killing the virus<br>whith a vaccine, don't let yourself and your<br>family get infected<br></p>
+        </div>
+    </div>
+    <div id="espaciados">
+        <img class="icon-block1" src="img/shieldGolden.png">
+        <p class="icon-block1">Best protection</p>
+        <img class="icon-block1" src="img/starGolden.png">
+        <p class="icon-block1">Select Vaccines</p>
+    </div>
+
+    <div id="espaciados">
+        <div class="schedule">
+            <img src="img/clock.png">
+            <p>Schedule your vaccinations</p>
+        </div>
+    </div>
+</div>
+
+<div class="contenedor">
+    <img id="farma" src="img/farmaceuticas.png">
+</div>
+
+<div class="contenedor">
+    <div id="question">
+        <p class="why" id="question-title">Why should I vaccine?</p>
+    <p class="why" id="question-body">In order to avoid doubts about getting the COVID-19<br>vaccine, identify the following 4 beneffits of COVID-19<br>vaccination:</p>
+    </div>
+</div>
+<div class="container">
+    <div class="box">
+        <div><img class="bloques" src="img/bloque1.png"></div>
+        <div><h3 class="bloques-title">Minimize the<br>spread of viruses</h3></div>
+        <div><p class="bloques-paragraph">Vaccine has been proven<br>effective to prevent someone<br>from getting infected with<br>Coronavirus.</p></div>
+    </div>
+    <div class="box">
+        <div><img class="bloques" src="img/shieldBlue.png"></div>
+        <div><h3 class="bloques-title">Forming<br>antibidies</h3></div>
+        <div><p class="bloques-paragraph">COVID-19 vaccine proven to<br>help shape antibody<br>response for immune system.</p></div>
+    </div>
+    <div class="box">
+        <div><img class="bloques" src="img/people.png"></div>
+        <div><h3 class="bloques-title">Protecting<br>people nearby</h3></div>
+        <div><p class="bloques-paragraph">The benefits of COVID-19<br>vaccination are the vaccine<br>that we get can also help<br>protect people around us.</p></div>
+    </div>
+    <div class="box">
+        <div><img class="bloques" src="img/graphic.png"></div>
+        <div><h3 class="bloques-title">Creating group<br>immunity</h3></div>
+        <div><p class="bloques-paragraph">COVID-19 vaccination is also<br>benefical for creating group<br>immunity or head immunity.</p></div>
+    </div>
+</div>
+<div class="contenedor">
+    <div class="pulgar-section">
+            <img src="img/chicaPulgar.png">
+    </div>
+    <div class="pulgar-section" id="pulgar-section-text">
+        <h2>Preparations Before Vaccine</h2>
+        <p class="preparations">The success of vaccines is strongly influenced by the<br>strenght of the body's immune system. Therefore, there are<br>severalthings that can be tried to make the COVID-19<br>vaccine work:</p>
+    </div>
+    <div id="pulgar-section-text">
+        <div class="acordeon">
+            <div class="acordeon__item">
+                <input type="radio" name="acordeon" id="acordeon1"><label for="acordeon1" class="acordeon__titulo">Avoid alcoholic beverages</label>
+                <p class="acordeon__contenido">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio quasi doloremque, ipsam aspernatur nisi mollitia, dolore, beatae ipsum cupiditate consectetur numquam animi. Debitis perspiciatis fuga, quos reprehenderit tempore quisquam?</p>
+            </div>
+            <div class="acordeon__item">
+                <input type="radio" name="acordeon" id="acordeon2"><label for="acordeon2" class="acordeon__titulo">Avoid stress</label>
+                <p class="acordeon__contenido">Stress has a profound effect on immune work.<br>Moreover, prolonged stress can increase cortisol<br>production and oxidative stress in the body.</p>
+            </div>
+            <div class="acordeon__item">
+                <input type="radio" name="acordeon" id="acordeon3"><label for="acordeon3" class="acordeon__titulo">Eath healthy food</label>
+                <p class="acordeon__contenido">Cupiditate nobis at incidunt ea officia eveniet impedit quaerat quisquam vero, excepturi dolores atque necessitatibus exercitationem numquam, totam neque quasi sint assumenda maiores iure tempora? Deserunt iusto incidunt debitis deleniti!</p>
+            </div>
+            <div class="acordeon__item">
+                <input type="radio" name="acordeon" id="acordeon4"><label for="acordeon4" class="acordeon__titulo">Get enough sleep</label>
+                <p class="acordeon__contenido">Eveniet voluptates neque cumque similique. Iste dolorum, quasi quis ad nam incidunt autem cum asperiores vitae, voluptatem dicta necessitatibus adipisci, inventore sed tempora ullam corporis laudantium odio cupiditate accusamus amet!</p>
+            </div>
+            <div class="acordeon__item">
+                <input type="radio" name="acordeon" id="acordeon5"><label for="acordeon5" class="acordeon__titulo">Exercise or physical activity</label>
+                <p class="acordeon__contenido">Eveniet voluptates neque cumque similique. Iste dolorum, quasi quis ad nam incidunt autem cum asperiores vitae, voluptatem dicta necessitatibus adipisci, inventore sed tempora ullam corporis laudantium odio cupiditate accusamus amet!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contenedor">
+    <section class="emergency">
+        <div id="emergancy-content" class="emergency">
+            <h2>Emergency Contact</h2>
+            <p>Contact one of the contacts below if you or your family feel<br>unwell and have similar symptoms such as Covid-19, make<br>sure you also take care of yourself before reporting to us:</p>
+        </div>
+        <img class="emergency" id="simi" src="img/SimiFinal.png">
+    </section>
+</div>
+<div class="contenedorV2">
+    <footer class="pie-pagina">
+        <div class="grupo1">
+            <div class="box2">
+                <h2>Vaccination</h2>
+                <p>Our goal is to help the<br>world free from the<br>ongoing pandemic</p>
+            </div>
+            <div class="box1">
+                <a href="#"><h2>About</h2></a>
+                <a href="#"><p>About Us</p></a>
+                <a href="#"><p>Features</p></a>
+                <a href="#"><p>News & Blog</p></a>
+            </div>
+            <div class="box1">
+                <a href="#"><h2>Company</h2></a>
+                <a href="#"><p>How We Work?</p></a>
+                <a href="#"><p>Capital</p></a>
+                <a href="#"><p>Security</p></a>
+            </div>
+            <div class="box1">
+                <a href="#"><h2>Support</h2></a>
+                <a href="#"><p>FAQs</p></a>
+                <a href="#"><p>Support Center</p></a>
+                <a href="#"><p>Contact Us</p></a>
+            </div>
+        </div>
+        <div class="grupo2">
+            <small id="derechos"><b>&copy; 2021 Vaccinations. All rights reserverd</b></small>
+            <small id="terminos"><b><a href="#">Terms & Agreements  Privacy Policy</a></b></small>
+        </div>
+    </footer>
+</div>
+
+</body>
+</html>
